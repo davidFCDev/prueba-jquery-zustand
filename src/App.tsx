b1af7b0b2773f661050895079cf297b1f805ddf4
David Fernández <davidfernandezcomesana@gmail.com>
2023-05-29 11:28:28 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,53 @@ import { useFavouriteReposStore } from './store/favouriteRepos';
 import { useState } from 'react';
 
 function App() {
-	const { data, isLoading } = useFetchRepositories('Dawid-Klos');
+	const [searchUser, setSearchUser] = useState('');
+	const [isSearchClicked, setIsSearchClicked] = useState(false);
+	const { data } = useFetchRepositories(searchUser);
 	const { favouriteReposId } = useFavouriteReposStore();
 
-	if (isLoading) return <div>Loading...</div>;
+	const handleSearch = (e: any) => {
+		e.preventDefault(); // Evitar que el formulario se envíe automáticamente
+		setIsSearchClicked(true);
+	};
 
 	return (
 		<section>
 			<Navbar />
-			<div className='flex flex-col w-full min-h-screen py-10 px-10 gap-10 justify-center'>
+			<div className='flex flex-col w-full min-h-screen py-10 px-10 gap-10'>
 				<div className='flex flex-col gap-2'>
-					<label>Search user</label>
-					<input
-						type='text'
-						placeholder='user'
-						value={'Dawid-Klos'}
-						className='p-2 rounded-sm focus:outline-none w-52 text-neutral-800 bg-neutral-200'
-					/>
+					<form onSubmit={handleSearch}>
+						{' '}
+						{/* Utilizar onSubmit en el formulario */}
+						<label>Search user</label>
+						<input
+							type='text'
+							placeholder='user'
+							value={searchUser}
+							onChange={e => setSearchUser(e.target.value)}
+							className='p-2 rounded-sm focus:outline-none w-52 text-neutral-800 bg-neutral-200'
+						/>
+						<button
+							type='submit' // Especificar el tipo del botón como "submit"
+							className='bg-green-300 text-neutral-800 p-2 rounded-sm w-52'
+						>
+							Search
+						</button>
+					</form>
 				</div>
 
-				<div className='grid grid-cols-4 gap-8'>
-					{data?.map((repository: Repository) => (
-						<Card
-							repository={repository}
-							key={repository.id}
-							isFavourite={favouriteReposId.includes(repository.id)}
-						/>
-					))}
-				</div>
+				{isSearchClicked && (
+					<div className='grid grid-cols-4 gap-8'>
+						{data &&
+							data.map((repository: Repository) => (
+								<Card
+									repository={repository}
+									key={repository.id}
+									isFavourite={favouriteReposId.includes(repository.id)}
+								/>
+							))}
+					</div>
+				)}
 			</div>
 		</section>
 	);
--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -3,11 +3,15 @@ import { useQuery, QueryFunctionContext } from '@tanstack/react-query';
 import { Repository } from './types';
 
 async function fetchRepos(ctx: QueryFunctionContext) {
-	const [_, githubUser] = ctx.queryKey;
-	const { data } = await api.get<Repository>(`/users/${githubUser}/repos`);
-	return data;
+  const [_, githubUser] = ctx.queryKey;
+  if (typeof githubUser !== 'string' || githubUser.trim() === '') {
+    // Si el nombre de usuario no es una cadena de texto válida o está vacío, retornar un array vacío
+    return [];
+  }
+  const { data } = await api.get<Repository>(`/users/${githubUser}/repos`);
+  return data;
 }
 
 export function useFetchRepositories(githubUser: string) {
-	return useQuery(['repos', githubUser], fetchRepos);
+  return useQuery(['repos', githubUser], fetchRepos);
 }
